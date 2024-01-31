558219901394868992a586515dcadb77f133e0f0
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-31 08:56:12 -0800

    update code

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * height - measures the height of a binary tree
+ * @tree: the root of the binary tree
+ * Return: the height | 0
+ */
+size_t height(const binary_tree_t *tree)
+{
+	size_t height1, height2;
+
+	if (tree == NULL)
+		return (0);
+
+	height1 = height(tree->left);
+	height2 = height(tree->right);
+	if (!(tree->left || tree->right))
+	{
+		height1--;
+		height2--;
+	}
+	if (height1 > height2)
+		return (++height1);
+	else
+		return (++height2);
+}
+
+
+/**
  * binary_tree_balance - measures the balance factor
  * 			of a binary tree
  * @tree: the root of the binary tree
@@ -13,8 +39,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	
-	heightL = binary_tree_height(tree->left);
-	heightR = binary_tree_height(tree->right);
+	heightL = height(tree->left);
+	heightR = height(tree->right);
 
 	return (heightL - heightR);
 }
