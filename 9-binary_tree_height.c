cf746f69acda0490a78cc8e3e462a47e622c7a16
Duaa Rabie <Duaarabie11@gmail.com>
2024-01-31 06:32:43 -0800

    update code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,12 +8,14 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
 	int height1 = 0, height2 = 0;
+	static int heightL, heightR;
 
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left)
 	{
+		heightL = height1;
 		height1++;
 		height1 += binary_tree_height(tree->left);
 		height1 += binary_tree_height(tree->right);
@@ -25,6 +27,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	
 	if (tree->right)
 	{
+		heightR = height2;
 		height2++;
 		height2 += binary_tree_height(tree->right);
 		height2 += binary_tree_height(tree->left);
@@ -34,7 +37,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 			return (height2);
 	}
 
-	if (height1 > height2)
-		return (height1);
-	return (height2);
+	if (heightL > heightR)
+		return (heightL);
+	return (heightR);
 }
