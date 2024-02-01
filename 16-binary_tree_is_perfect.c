fc969aff6467a525c50e83e061483100b4fb9dc4
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-01 02:51:42 -0800

    update code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,7 +15,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree->left == NULL && tree->right == NULL)
 		return (0);
 
-	if (tree->left || tree->right)
+	if (tree->left && tree->right)
 	{
 		result1 = binary_tree_is_perfect(tree->left);
 		result2 = binary_tree_is_perfect(tree->right);
