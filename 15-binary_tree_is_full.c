ba1412562eaa75635b2b3e7fa16cbd1f241d9529
Duaa Rabie <Duaarabie11@gmail.com>
2024-02-01 02:08:46 -0800

    update code

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,14 +12,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left && tree->right)
+	if ((tree->left && tree->right) || (!tree->left && !tree->right))
 	{
 		result1 = binary_tree_is_full(tree->left);
 		result2 = binary_tree_is_full(tree->right);
-		if (result1 || result2)
-			return (0);
-		else
+		if (!result1 && !result2)
 			return (1);
+		if (result1 && result2)
+			return (1);
+		else
+			return (0);
 	}
 	return (0);
 }
